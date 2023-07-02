cf1122e4af9d2efaff1a2a7dce10c1b2835de405
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-07-03 00:06:19 +0700

    feat: update twitter dependency

--- src/methods/broadcasts/twitter.js
+++ src/methods/broadcasts/twitter.js
@@ -1,12 +1,12 @@
-const TwitterClient = require('twitter-api-client').TwitterClient;
+const { TwitterApi } = require('twitter-api-v2');
 
 const { toArray } = require('../../utils');
 
 module.exports = async data => {
-  const twitter_client = new TwitterClient({ apiKey: process.env.TWITTER_KEY, apiSecret: process.env.TWITTER_SECRET, accessToken: process.env.TWITTER_ACCESS_TOKEN, accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET });
+  const twitterClient = new TwitterApi({ appKey: process.env.TWITTER_KEY, appSecret: process.env.TWITTER_SECRET, accessToken: process.env.TWITTER_ACCESS_TOKEN, accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET });
   toArray(data).forEach(async d => {
     try {
-      await twitter_client.tweets.statusesUpdate({ status: d });
+      await twitterClient.v2.tweet(d);
     } catch (error) {}
   });
 };
